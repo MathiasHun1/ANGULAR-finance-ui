1475012b0f20ffdd13102a541e4a6c0c94d154a9
snus4me <mathias.hun@gmail.com>
2025-08-14 15:48:05 +0200

    working on budgets page

--- src/app/pages/budgets/budgets.ts
+++ src/app/pages/budgets/budgets.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, effect, inject } from "@angular/core";
+import { ApiService } from "../../services/api-service";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { TransactionModel } from "../../models/models";
 
 @Component({
-  selector: 'app-budgets',
-  imports: [],
-  templateUrl: './budgets.html',
-  styleUrl: './budgets.scss'
+  selector: "app-budgets",
+  imports: [CommonModule],
+  templateUrl: "./budgets.html",
+  styleUrl: "./budgets.scss",
 })
 export class Budgets {
+  apiService = inject(ApiService);
 
+  transactions = toSignal<TransactionModel[]>(
+    this.apiService.getTransactions()
+  );
+  budgets = toSignal(this.apiService.getBudgets());
+
+  filterTransactions(value: string) {
+    const transactions = this.transactions();
+    if (!transactions) {
+      return;
+    }
+    return transactions.filter((t) => t.category === value);
+  }
 }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,23 +1,32 @@
 import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map, tap } from "rxjs";
-import { DataModel, TransactionModel } from "../models/models";
+import { BudgetModel, TransactionModel } from "../models/models";
 
 @Injectable({
   providedIn: "root",
 })
 export class ApiService {
+  private baseUrl = "http://localhost:3000";
   private http = inject(HttpClient);
 
   getTransactions() {
-    return this.http.get<DataModel>("/data.json").pipe(
-      map((result) =>
-        result.transactions.map((transaction: TransactionModel) => ({
-          ...transaction,
-          avatar: transaction.avatar.replace("./assets", ""),
-        }))
-      ),
-      tap((result) => console.log("logged from service: ", result))
-    );
+    return this.http
+      .get<TransactionModel[]>(`${this.baseUrl}/transactions`)
+      .pipe(
+        tap((result) => console.log("logged from API service: ", result)),
+        map((result) =>
+          result.map((transaction: TransactionModel) => ({
+            ...transaction,
+            avatar: transaction.avatar.replace("./assets", ""),
+          }))
+        )
+      );
+  }
+
+  getBudgets() {
+    return this.http
+      .get<BudgetModel[]>(`${this.baseUrl}/budgets`)
+      .pipe(tap((result) => console.log("Budgets from API: ", result)));
   }
 }
