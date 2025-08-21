b0218e67efa37768d94cb8a886929a0c49fb2414
snus4me <mathias.hun@gmail.com>
2025-08-21 17:50:50 +0200

    working on overview page

--- src/app/pages/overview/overview.ts
+++ src/app/pages/overview/overview.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { ApiService } from "../../services/api-service";
 import { toSignal } from "@angular/core/rxjs-interop";
-import { BalanceModel } from "../../models/models";
+import { BalanceModel, TransactionModel } from "../../models/models";
 import { CommonModule } from "@angular/common";
 import { PotsService } from "../../services/pots-service";
 import { Router, RouterLink } from "@angular/router";
 import { ListItem } from "../../shared/components/list-item/list-item";
+import { TransactionService } from "../../services/transaction-service";
 
 @Component({
   selector: "app-overview",
@@ -16,8 +17,20 @@ import { ListItem } from "../../shared/components/list-item/list-item";
 export class Overview {
   apiService = inject(ApiService);
   potsService = inject(PotsService);
+  transactionsService = inject(TransactionService);
 
+  //pots related data
   balance = toSignal<BalanceModel>(this.apiService.getBalance());
   totalSavedPots = this.potsService.totalSavedPots;
   potsData = this.potsService.pots;
+
+  //transactions related data
+  transactions = computed<TransactionModel[]>(() => {
+    const transactions = this.transactionsService.computedTransactions();
+    if (!transactions) {
+      return [];
+    }
+
+    return transactions.slice(0, 5);
+  });
 }
